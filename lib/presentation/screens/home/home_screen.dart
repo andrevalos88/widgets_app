377afe32773ea288dacac1de721e294bae785fa9
Jaime <jmejia@edcus.com>
2024-04-21 23:02:00 -0600

    go router configurado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -58,7 +59,8 @@ class _CustomListTile extends StatelessWidget {
         //     builder: (context) => const ButtonsScreen(),
         //   ),
         // );
-        Navigator.of(context).pushNamed(menuItem.link);
+        //Navigator.of(context).pushNamed(menuItem.link);
+        context.push(menuItem.link);
       },
     );
   }
